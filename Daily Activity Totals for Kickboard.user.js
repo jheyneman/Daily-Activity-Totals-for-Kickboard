bc6f939836bde3b96ba8f7ab1827f622b14cc3d7
James Heyneman <heyneman.james@gmail.com>
2016-09-12 21:30:04 -0400

    Move totals to sidebar.

--- Daily Activity Totals for Kickboard.user.js
+++ Daily Activity Totals for Kickboard.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Daily Activity Totals for Kickboard
-// @version      0.2
+// @version      0.3
 // @description  Provides student absent/present counts on Kickboard for United Schools Network
 // @author       James Heyneman
 // @match        https://united.kickboardforteachers.com/culture/daily-activity
@@ -10,44 +10,35 @@
 
 $(document).ready(function() {
 
-  var students_ul = document.getElementById('students');
-
-  var totals_li = document.createElement('li');
-
-  totals_li.innerHTML = '<div class="details">' +
-      '<div class="detail clear">' +
+  $( "#sidebar" ).append('<div class="widget clear">' +
           '<div style="text-align: center;">' +
                 '<input type="button" value="Get Totals" id="getTotals" style="background-color: #95be22; color: #fff; border-radius: 4px; font-size: 16px; border: none; padding: 16px;">' +
                '</span>' +
           '</div>' +
       '</div>' +
-   '</div>';
+        '<div class="widget clear" id="totalsDiv"></div>' );
 
-   students_ul.appendChild(totals_li);
+ $('#getTotals').click(function(){
+  var totals_div = document.getElementById('totalsDiv');
 
-
-$('#getTotals').click(function(){
+  var student_list_elements = document.getElementsByClassName("detail clear");
   var present_elements = document.getElementsByClassName("present");
   var present_elements_hidden = document.getElementsByClassName("present hidden");
   var absent_elements = document.getElementsByClassName("absent");
   var absent_elements_hidden = document.getElementsByClassName("absent hidden");
   var present_students = present_elements.length - present_elements_hidden.length;
   var absent_students = absent_elements.length - absent_elements_hidden.length;
+  var total_students = student_list_elements.length;
 
-  totals_li.innerHTML =
-   '<div class="details">' +
-      '<div class="detail clear">' +
-          '<div style="margin-left: auto; margin-right: auto; width: 75%;">' +
-               '<span style="font-weight:bold; font-size: 18px; color: #222;">' +
-                     '<div style="float: left; width: 33%; text-align: center;">' + 'Total: ' + (present_students + absent_students) + '</div>' +
+  totals_div.innerHTML = '<div class="widget clear">' +
+          '<div style="margin-left: auto; margin-right: auto; width: 100%;">' +
+               '<span style="font-weight:bold; font-size: 14px; color: #222;">' +
+                     '<div style="float: left; width: 33%; text-align: center;">' + 'Total: ' + total_students + '</div>' +
                      '<div style="float: left; width: 33%; text-align: center; border-left: 1px solid black; border-right: 1px solid black;">Present: ' + present_students + '</div>' +
                      '<div style="float: left; width: 33%; text-align: center;">Absent: ' + absent_students + '</div>' +
                '</span>' +
           '</div>' +
-      '</div>' +
-   '</div>';
-
-  students_ul.appendChild(totals_li);
+      '</div>';
 });
 
 });
